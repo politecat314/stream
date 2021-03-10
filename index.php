84c59e728960e31ec3995159d56cc9ff5921db46
Aman <ahmedamanibrahim@gmail.com>
2021-03-10 18:15:10 +0500

    Styling done

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ include 'ffmpeg.php';
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
     <div class="container">
             <a class="navbar-brand" href="#">Stream</a>
             <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
