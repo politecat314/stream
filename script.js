8d9503bfe6b624dec587d8ba3d3ff674d019a5f6
Aman <ahmedamanibrahim@gmail.com>
2021-03-12 14:00:03 +0500

    links open in new tab

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ window.onload = () => {
                 video_name = video.parentNode.children[1].children[0].innerHTML;
 
                 if (watched.includes(video_name)) {
-                    video.parentNode.style.backgroundColor = "#1f1e52";
+                    video.parentNode.style.backgroundColor = "#0d0d24";
                 }
                 
             }
