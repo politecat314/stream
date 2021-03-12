9248bdd1e4f9faa8c1ff395305f0112ccf930f54
Aman <ahmedamanibrahim@gmail.com>
2021-03-12 18:57:57 +0500

    Downloads folder added

--- htmlGenerators.php
+++ htmlGenerators.php
@@ -5,7 +5,11 @@ function videoColGenerate($path_to_folder, $video_name) {
     // video name must contain extension like: example.mp4
     echo '<div class="col col-lg-3 col-sm-12 d-flex justify-content-center videoCol">';
     echo    '<div class="card" style="width: 18rem;">'; 
-    echo    '    <a target="_blank" class="videoLink" href="watch.php?path='.$path_to_folder.'\\'.$video_name.'">';
+    if (str_ends_with($video_name, ".mkv")) {
+        echo    '    <a class="videoLink" href="'.$path_to_folder.'\\'.$video_name.'">';    
+    } else {
+        echo    '    <a target="_blank" class="videoLink" href="watch.php?path='.$path_to_folder.'\\'.$video_name.'">';
+    }
     echo    '        <img src="" class="card-img-top" alt="No preview available">';
     echo    '    </a>';
     echo    '    <div class="card-body">';
@@ -13,6 +17,24 @@ function videoColGenerate($path_to_folder, $video_name) {
     echo    '    </div>';
     echo    '</div>';
     echo '</div>';
+}
+
+
+function generateContainerFolder($title, $video_files, $folder_path) {
+    echo '<div class="container folder">
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <h2>' . $title . '</h2>
+                </div>
+            </div>
+        
+            <div class="row">';
+                foreach ($video_files as $key => $video_name) { // video_name contains format like: example.mp4  
+                    videoColGenerate($folder_path, $video_name);
+                }
+
+    echo '  </div>
+        </div>';
 }
 
 
--- index.php
+++ index.php
@@ -4,13 +4,6 @@ include 'htmlGenerators.php';
 include 'db_connection.php';
 include 'ffmpeg.php';
 
-// $filePath = 'C:\Users\user\Downloads\Parks.And.Recreation.S02.COMPLETE.WEB-DL.x264-LeRalouf\Parks.And.Recreation.S02E22.Telethon.WEB-DL.x264-LeRalouf.mp4';
-// $stream = new VideoStream($filePath);
-// $stream->start();
-
-
-
-
 ?>
 
 <html>
@@ -27,17 +20,17 @@ include 'ffmpeg.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <!-- font awesome css -->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 
 
 
-    <link rel="icon" href="iconfinder_play_alt_118620.ico"/>
+    <link rel="icon" href="iconfinder_play_alt_118620.ico" />
     <title>Stream</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-    <div class="container">
+        <div class="container">
             <a class="navbar-brand" href="#">Stream</a>
             <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -55,48 +48,40 @@ include 'ffmpeg.php';
     </nav>
 
     <?php
-    
 
-
+    // start of load folders in the database
     $conn = OpenCon();
 
-                $sql = "SELECT * FROM stream
+    $sql = "SELECT * FROM stream
                         ORDER BY pk DESC";
-                $result = $conn->query($sql);
-                
-                if ($result->num_rows > 0) {
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-                        $title = $row['title'];
-                        $folder_path = $row['path'];
-                        
-                        $video_files = array_values(array_filter(scandir($folder_path), "validFormat")); // return all mp4 and mkv files in $folder_path
-                        
-                        
-                        echo
-        '<div class="container folder">
-            <div class="row">
-                <div class="col d-flex justify-content-center">
-                    <h2>' . $title . '</h2>
-                </div>
-            </div>
-            
-            <div class="row">';
-        foreach ($video_files as $key => $video_name) { // video_name contains format like: example.mp4
-            
-            videoColGenerate($folder_path, $video_name);
-        }
+    $result = $conn->query($sql);
 
-        echo "</div>";
-        echo "</div>";
-                      
-                    }
-                  } else {
-                    echo "No urls in the database";
-                  }
-                
-                
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            $title = $row['title'];
+            $folder_path = $row['path'];
+
+            $video_files = array_values(array_filter(scandir($folder_path), "validFormat")); // return all mp4 and mkv files in $folder_path
+
+            generateContainerFolder($title, $video_files, $folder_path);
+        }
+    } else {
+        echo "No urls in the database";
+    }
+
     CloseCon($conn);
+    // end of load folders in the database
+
+    // getting things inside downloads folder
+    $downloads_folder = scandir('./Downloads');
+
+    for ($i=2; $i<count($downloads_folder); $i++){
+        $folder_path = './Downloads/'.$downloads_folder[$i];
+        $video_files = array_values(array_filter(scandir($folder_path), "validFormat")); // return all mp4 and mkv files in $folder_path
+
+        generateContainerFolder($downloads_folder[$i]. ' <i class="fas fa-download"></i>', $video_files, $folder_path);
+    }
 
     ?>
 
@@ -107,10 +92,10 @@ include 'ffmpeg.php';
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
-
+    <script src="script.js"></script>
 
 </body>
 
-<script src="script.js"></script>
+
 
 </html>
